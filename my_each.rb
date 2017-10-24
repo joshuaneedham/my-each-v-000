14b1f4ca0fe36302a2a50d12e57970c7d02ad9d4
Joshua Needham <joshuabneedham@gmail.com>
2017-10-24 22:04:01 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -2,9 +2,10 @@ def my_each(file)
   # code here
   if block_given?
     file.each do |n|
-    puts "#{n}"
-    yield
-  else
-    file
+      puts "#{n}"
+      yield
+    else
+      file
+    end
   end
 end
