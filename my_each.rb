25ccd21ab07e16a120333b44079355af4f5aa54a
Joshua Needham <joshuabneedham@gmail.com>
2017-10-24 22:32:47 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,10 +1,16 @@
 def my_each(file)
-  if block_given?
-    file.each {|name| puts "#{name}" }
+  i = 0
+  while i < file.length
+    yield(file[i])
+    i += 1    
   end
 end
 
 
 # my_each do |name|
 #   puts "#{name}"
+# end
+
+# if block_given?
+#   file.each { |name| puts "#{name}" }
 # end
